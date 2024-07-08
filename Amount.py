488e4331fedde7680b8405395a4946b042263552
Odunsi Tomi <officialtomboy100@gmail.com>
2024-07-08 12:03:09 +0100

    edit commit modified:   Amount.py

--- Amount.py
+++ Amount.py
@@ -1,1 +1,24 @@
-
+def amount():
+    amount = []
+    
+    while True:
+        print("\n MENU " )
+        print("1. Record the Amount of the drugs")
+        print("2. Edit the amount of the drugs")
+        print("3. Quit ")
+        
+        
+        choice = input("Enter your choice (1 or 2 ): ").strip()
+        
+        if choice == '1':
+            record_amount(amount)
+        elif choice == '2':
+            edit_amount(amount)
+        elif choice == '3':
+            break
+        else :
+            print("This is an invalid option ")
+            
+def record_amount(amount):
+    while True:
+        
